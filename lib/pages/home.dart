18c7a968f375a05004542b2c2fda8fe8036b96dd
priyadarshii786 <tathagatp918@gmail.com>
2024-09-16 01:18:48 +0530

    C-04; Added the categories section

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,6 +9,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List chikku = [
+    "images/laptop.png",
+    "images/TV.png",
+    "images/headphone_icon.png",
+    "images/watch.png"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,17 +63,86 @@ class _HomeState extends State<Home> {
               width: MediaQuery.of(context).size.width,
               child: TextField(
                 decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Search Products",
-                    hintStyle: AppWidget.lightTextFieldStyle(),
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.black,
-                    )),
+                  border: InputBorder.none,
+                  hintText: "Search Products",
+                  hintStyle: AppWidget.lightTextFieldStyle(),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                ),
               ),
             ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Categories",
+                  style: AppWidget.semiboldTextFieldStyle(),
+                ),
+                Text(
+                  "See all",
+                  style: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20.0),
+              height: 140,
+              child: ListView.builder(
+                  itemCount: chikku.length,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return CategoryTile(image: chikku[index]);
+                  }),
+            )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  // const CategoryTile({super.key});
+  String image;
+  CategoryTile({super.key, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 20.0),
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(50),
+      ),
+      height: 90,
+      width: 90,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+        children: [
+          Image.asset(
+            image,
+            height: 50,
+            width: 50,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Icon(Icons.arrow_forward_outlined)
+        ],
       ),
     );
   }
