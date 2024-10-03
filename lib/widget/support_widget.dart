e690e0e11dafa263ee4db07c74a975278247a68b
priyadarshii786 <tathagatp918@gmail.com>
2024-10-04 04:00:09 +0530

    C-05; Changed fontsize in this file

--- lib/widget/support_widget.dart
+++ lib/widget/support_widget.dart
@@ -7,16 +7,16 @@ import 'package:flutter/material.dart';
 class AppWidget {
   static TextStyle boldTextFieldStyle() {
     return TextStyle(
-        color: Colors.black, fontSize: 28.0, fontWeight: FontWeight.bold);
+        color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold);
   }
 
   static TextStyle lightTextFieldStyle() {
     return TextStyle(
-        color: Colors.black54, fontSize: 20.0, fontWeight: FontWeight.w500);
+        color: Colors.black54, fontSize: 15.0, fontWeight: FontWeight.w500);
   }
 
   static TextStyle semiboldTextFieldStyle() {
     return TextStyle(
-        color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold);
+        color: Colors.black, fontSize: 15.0, fontWeight: FontWeight.bold);
   }
 }
